4728d213a1fd6eb5beaa9c2c26b06c65d2553634
Radhofan <radhofanazizi@gmail.com>
2025-03-11 16:47:52 +0700

    ada

--- reproduce.sh
+++ reproduce.sh
@@ -10,7 +10,7 @@ export PATH="$HOME/miniconda/bin:$PATH"
 # Set Up Conda env 
 conda create -n DevMuT python=3.9 -y
 conda activate DevMuT
-conda install --file DevMuT/code/DevMuT/requirments.txt -y
+conda install --file DevMuT/code/DevMuT/requirments.txt -c conda-forge -y
 
 # Set environment variables
 export CONTEXT_DEVICE_TARGET=GPU
