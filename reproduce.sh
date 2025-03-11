71e35cbbe90f38fc26248744f834a72e05325ab8
Radhofan <radhofanazizi@gmail.com>
2025-03-11 21:52:29 +0700

    ada

--- reproduce.sh
+++ reproduce.sh
@@ -10,8 +10,9 @@ export PATH="$HOME/miniconda/bin:$PATH"
 # Set Up Conda env 
 conda create -n DevMuT python=3.9 -y
 conda activate DevMuT
-conda install --file DevMuT/code/DevMuT/requirments.txt -c conda-forge -y
-pip install absl-py==0.15.0
+# conda install --file DevMuT/code/DevMuT/requirments.txt -c conda-forge -y
+# pip install absl-py==0.15.0
+pip install -r DevMuT/code/DevMuT/requirements.txt
 pip install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia -y
 
 # Set environment variables
