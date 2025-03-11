11899c9113adf7c63506365e9f98bc1c45304549
Radhofan <radhofanazizi@gmail.com>
2025-03-11 22:18:54 +0700

    ada

--- reproduce.sh
+++ reproduce.sh
@@ -10,6 +10,7 @@ export PATH="$HOME/miniconda/bin:$PATH"
 # Set Up Conda env 
 conda create -n DevMuT python=3.9 -y
 conda init
+exec bash
 conda activate DevMuT
 pip install --upgrade pip setuptools wheel
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
